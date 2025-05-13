68df55e1b444837b6daa5895d9e8270f5d33a209
Francisco Teran <82130722+FranciscoTeran2001@users.noreply.github.com>
2025-05-13 15:56:10 -0500

    Delete backend directory

--- backend/src/main/java/com/liceo/notas/exceptions/EmailInvalidoException.java
+++
@@ -1,9 +0,0 @@
-package com.liceo.notas.exceptions;
-
-
-
-public class EmailInvalidoException extends RuntimeException {
-    public EmailInvalidoException(String message) {
-        super(message);
-    }
-}
